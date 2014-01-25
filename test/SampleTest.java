3b3826b00ee5b8ea700fe006c0cddc1e429c1246
Kajal <kajal@inankhome.com>
2014-01-25 15:59:59 +0530

    Sample Test for Gradle

---
+++ test/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by kajaljad on 1/25/14.
+ */
+public class SampleTest {
+    @Test
+    public void sampletestforgradle(){
+        assertEquals(1,1);
+    }
+}
